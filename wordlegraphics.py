1f118e8ebc9f16b20f03147479922f50714ef868
Antmnguyen <116772578+Antmnguyen@users.noreply.github.com>
2023-08-22 15:39:41 -0700

    Update wordlegraphics.py

--- wordlegraphics.py
+++ wordlegraphics.py
@@ -7,22 +7,23 @@ counter = 0
 
 def process_input(event):
     global counter
-    guess = myentry.get()
-    info = compare(guess,word_info,word_list[index])  #make it so compare will give info on each char editing i for each
-    for i in range(len(guess)):
-        update_specific_label(counter, i, guess[i],info)
-    myentry.delete(0, tk.END) # deletes the entry once its entered
+    user_input = myentry.get()
+    if user_input not in word_list:
+        return
+    info = compare(user_input,word_info,word_list[index])  #make it so compare will give info on each char editing i for each
+    for i in range(len(user_input)):
+        update_specific_label(counter, i, user_input[i],info)
     counter += 1
-
+    myentry.delete(0, tk.END)
 def update_specific_label(row, column, char,info):
     specific_label = labels[row][column]
     char = char.upper()
     if info[column] == 0:
-        specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="green")
+        specific_label.config(text=char,fg="white", font=('Arial', 10, "bold"), background="green")
     elif info[column] == 1:
-        specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="#ffc425")
+        specific_label.config(text=char,fg="white", font=('Arial', 10, "bold"), background="#ffc425")
     else:
-        specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="grey")
+        specific_label.config(text=char,fg="white", font=('Arial', 10, "bold"), background="grey")
 
 root = tk.Tk()
 
