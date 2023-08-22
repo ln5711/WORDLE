c2bf11ac5898f0662985b2036db65e6cfd9d13d3
Antmnguyen <116772578+Antmnguyen@users.noreply.github.com>
2023-08-22 14:52:03 -0700

    Update wordlegraphics.py

--- wordlegraphics.py
+++ wordlegraphics.py
@@ -1,17 +1,23 @@
 import tkinter as tk
+import random
+import csv
+from algorithm import *
+global counter
+counter = 0
 
 def process_input(event):
-    word = myentry.get()
-    for i in range(len(word)):
-        update_specific_label(0, i, word[i],int)
+    guess = myentry.get()
+    info = compare(guess,word_info,word_list[index])  #make it so compare will give info on each char editing i for each
+    for i in range(len(guess)):
+        update_specific_label(counter, i, guess[i],info)
     myentry.delete(0, tk.END) # deletes the entry once its entered
 
 def update_specific_label(row, column, char,info):
     specific_label = labels[row][column]
-
-    if info == 0:
+    char = char.upper()
+    if info[column] == 0:
         specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="green")
-    elif info == 1:
+    elif info[column] == 1:
         specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="yellow")
     else:
         specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="grey")
@@ -37,7 +43,19 @@ for i in range(6):
     for j in range(5):
         labels[i][j].grid(row=i, column=j, padx=5, pady=5)
 
-myentry.bind("<Return>", process_input)
+#myentry.bind("<Return>", process_input)
 root.geometry("500x500")
 label.pack(padx=20, pady=20)
+
+
+word_list = read_csv() #reads csv and puts in list
+index = random.randint(0,495) #chooses index randomly for word
+word_info = save_info(word_list[index]) #saves information of specific word amounts of letter
+print(word_list[index])
+while counter < 5:
+    myentry.bind("<Return>", process_input)
+    #result = compare(guess,word_info,word_list[index])
+    counter +=1
+print(word_list[index])
+
 root.mainloop()
