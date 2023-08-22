09e47c5c7c112d116c70ff1a2d6749c20a0277de
ln5711 <142541699+ln5711@users.noreply.github.com>
2023-08-22 15:19:19 -0700

    Update wordlegraphics.py

--- wordlegraphics.py
+++ wordlegraphics.py
@@ -6,11 +6,13 @@ global counter
 counter = 0
 
 def process_input(event):
+    global counter
     guess = myentry.get()
     info = compare(guess,word_info,word_list[index])  #make it so compare will give info on each char editing i for each
     for i in range(len(guess)):
         update_specific_label(counter, i, guess[i],info)
     myentry.delete(0, tk.END) # deletes the entry once its entered
+    counter += 1
 
 def update_specific_label(row, column, char,info):
     specific_label = labels[row][column]
@@ -18,7 +20,7 @@ def update_specific_label(row, column, char,info):
     if info[column] == 0:
         specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="green")
     elif info[column] == 1:
-        specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="yellow")
+        specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="#ffc425")
     else:
         specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="grey")
 
@@ -52,10 +54,10 @@ word_list = read_csv() #reads csv and puts in list
 index = random.randint(0,495) #chooses index randomly for word
 word_info = save_info(word_list[index]) #saves information of specific word amounts of letter
 print(word_list[index])
-while counter < 5:
-    myentry.bind("<Return>", process_input)
-    #result = compare(guess,word_info,word_list[index])
-    counter +=1
+myentry.bind("<Return>", process_input)
+while counter < 6:
+    root.update_idletasks()
+    root.update()
 print(word_list[index])
 
 root.mainloop()
