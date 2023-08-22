65ade618444f36e482891c2a4a1b2f8e4a6be541
ln5711 <142541699+ln5711@users.noreply.github.com>
2023-08-22 15:19:38 -0700

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,63 +1,77 @@
-import tkinter as tk
-import random
 import csv
-from algorithm import *
-global counter
-counter = 0
+import random
 
-def process_input(event):
-    global counter
-    guess = myentry.get()
-    info = compare(guess,word_info,word_list[index])  #make it so compare will give info on each char editing i for each
-    for i in range(len(guess)):
-        update_specific_label(counter, i, guess[i],info)
-    myentry.delete(0, tk.END) # deletes the entry once its entered
-    counter += 1
-
-def update_specific_label(row, column, char,info):
-    specific_label = labels[row][column]
-    char = char.upper()
-    if info[column] == 0:
-        specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="green")
-    elif info[column] == 1:
-        specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="#ffc425")
-    else:
-        specific_label.config(text=char,fg="white", font=('Arial', 15, "bold"), background="grey")
-
-root = tk.Tk()
-
-root.geometry("1000x1000")
-root.title("WORDLE")
-
-label = tk.Label(root, text="WORDLE", font=('Arial', 50, "bold"))
-label.pack(padx=20, pady=20)
-
-myentry = tk.Entry(root)
-myentry.pack(padx=10, pady=10)
-
-# Create a frame for the grid of text boxes
-frame = tk.Frame(root)
-frame.pack(pady=20)  # Add padding around the frame
-
-# Create a 5x6 grid of text boxes within the frame
-labels = [[tk.Label(frame, width=10, height=5, borderwidth=2, relief="solid") for j in range(5)] for i in range(6)]
-for i in range(6):
-    for j in range(5):
-        labels[i][j].grid(row=i, column=j, padx=5, pady=5)
-
-#myentry.bind("<Return>", process_input)
-root.geometry("500x500")
-label.pack(padx=20, pady=20)
+def read_csv():
+    words = []
+    with open('5_letter_words.csv', mode='r') as file:
+        reader = csv.reader(file)
+        next(reader, None)
+        for row in reader:
+            words.append(row[0])
+    return words
 
 
-word_list = read_csv() #reads csv and puts in list
-index = random.randint(0,495) #chooses index randomly for word
-word_info = save_info(word_list[index]) #saves information of specific word amounts of letter
-print(word_list[index])
-myentry.bind("<Return>", process_input)
-while counter < 6:
-    root.update_idletasks()
-    root.update()
-print(word_list[index])
+def save_info(word):
+    letter_counts = {}
+    # Loop through each letter in the word
+    for letter in word:
+        # If the letter is already in the dictionary, increment its count by 1
+        if letter in letter_counts:
+            letter_counts[letter] += 1
+        # If the letter is not in the dictionary, add it with a count of 1
+        else:
+            letter_counts[letter] = 1
+    return letter_counts
 
-root.mainloop()
+
+def input_word(word_list):
+    while True:
+        user_input = input("Please enter a five letter word:"'\n').strip()
+        if user_input in word_list:
+            break
+        else:
+            print("thats not a five letter word" '\n')
+    return user_input
+
+
+def compare(g, winfo, w):
+    info = [2] * 5
+
+    # 1. Check for correct position
+    for index in range(len(g)):
+        if g[index] == w[index]:
+            info[index] = 0
+
+    # 2. Check if the letter exists anywhere in the word
+    for index, letter in enumerate(g):
+        if letter in w and info[index] != 0:  # check if the letter exists and hasn't been marked as correct
+            info[index] = 1
+
+    return info
+
+    for index, letter in enumerate(g):  # Using enumerate to get index and letter
+        if letter in winfo:  # if it has the letter
+            if w[index] == letter:  # check if it's the right position
+                info[index] = 0
+            else:
+                info[index] = 1
+
+    return info
+
+#word_list = read_csv() #reads csv and puts in list
+#index = random.randint(0,495) #chooses index randomly for word
+#word_info = save_info(word_list[index]) #saves information of specific word amounts of letter
+#counter = 0
+#print(word_list[index])
+#while counter < 6:
+#    guess = input_word() #make lowercase
+#    result = compare(guess,word_info,word_list[index])
+#   if result:
+#        print("correct")
+#        break
+#    else:
+#        print("try again")
+#    counter +=1
+#print(word_list[index])
+###
+
